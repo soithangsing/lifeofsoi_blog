423f47ad537ccc606514834beba0060e757f2ab9
soithangsing <getdatasoi@outlook.com>
2022-03-13 15:28:13 +0530

    Added back-end functionality to post comments

--- services/index.js
+++ services/index.js
@@ -135,5 +135,13 @@ return result.categories;
 }
 
 export const submitComment = async(obj) => {
-  
+  const result = await fetch('/api/comments', {
+    method:'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(obj),
+  });
+
+  return result.json();
 }
