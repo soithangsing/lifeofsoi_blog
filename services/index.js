a22a31e2e992e40737e9c3111099eaeafd36f652
soithangsing <getdatasoi@outlook.com>
2022-03-14 11:34:08 +0530

    updated photo to displayPhoto

--- services/index.js
+++ services/index.js
@@ -198,7 +198,7 @@ export const getCategoryPost = async (slug) => {
               bio
               name
               id
-              photo {
+              displayPhoto {
                 url
               }
             }
