ce2f446c2c4c08d3916f95e8a7567608a9ebc006
soithangsing <getdatasoi@outlook.com>
2022-03-14 11:07:52 +0530

    implemented post loader and post carousel

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 import { getPosts, getPostDetails } from '../../services';
-import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm} from '../../components'
+import { useRouter } from 'next/router'
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader} from '../../components'
+
 
 const PostDetails = ({ post }) => {
-    console.log(post);
+    const router = useRouter();
+    if (router.isFallback) {
+        return <Loader />
+    }
   return (
         <div className='container mx-auto px-10 mb-8'>
             <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
